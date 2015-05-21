608ca18d4d0abf2d88208d74d4cf4df18f87554f
DanielAsGold <goldan85@gmail.com>
2015-05-21 14:29:08 -0700

    I think its done

--- Informatics/R/spring.short.R
+++ Informatics/R/spring.short.R
@@ -26,16 +26,18 @@ spring.summary = function(clim, spring.months = c(4:6)) {
   #compute values
   mean.springT = mean(spring$tavg) #computes the mean daily average temp in spring months
   mean.springT=round(mean.springT,2) #Rounds my answer to two decimal places
-  AgTemp=aggregate(spring$tavg, by=list(spring$year), mean)
-  lowyear = AgTemp$year[which.min(AgTemp)] #identifies 
+  AgTemp=aggregate(spring$tavg, by=list(spring$year), mean) #aggregates the avg. temp data into average by year
+  lowyear = AgTemp$Group.1[which.min(AgTemp$x)] #identifies the year with the coldest spring (average daily temp across 3 months)
   
-  spring.precip = aggregate(spring$tmax, by=list(spring$year), sum)
+  spring.precip = aggregate(spring$rain, by=list(spring$year), sum)
 	
   
-  colnames(spring.precip) = c("year","precip")  
-  mean.spring.precip = mean(spring.precip$precip)
-  wettest.spring = spring.precip$year[which.max(spring.precip$precip)]
+  colnames(spring.precip) = c("Year","Rain")  
+  MeanSpringRain = mean(spring.precip$Rain)
+  MeanSpringRain=round(MeanSpringRain,2) #Rounds my answer to two decimal places
+  WettestSpring = spring.precip$Year[which.max(spring.precip$Rain)]
+  WettestSpring=round(WettestSpring,2) #Rounds my answer to two decimal places
   
-  return(list(mean.springT = mean.springT, coldest.spring=lowyear, 
-              mean.springP=mean.spring.precip,wettest.spring=wettest.spring ))
+  return(list("Mean Spring Temp" = mean.springT, "Coldest Spring"=lowyear, 
+              "Mean Spring Rain"=MeanSpringRain, "Wettest Spring"=WettestSpring ))
 }
