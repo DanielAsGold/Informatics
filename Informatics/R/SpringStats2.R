0f4eb064e4d8ba7fa77468cc017c63e1100023e6
DanielAsGold <goldan85@gmail.com>
2015-05-21 15:35:08 -0700

    almost

--- Informatics/R/SpringStats2.R
+++ Informatics/R/SpringStats2.R
@@ -1,24 +1,33 @@
-spring.summary = function(clim, spring.months = c(4:6)) {
+#creating a function called spring.smmary that summarizeds spring month data with the default interval of "month"
+spring.summary = function(clim, spring.months = c(4:6), Interval="m") {
     
-  # check to make sure data is in required format
-  requiredcols = c("tmax","tmin","year","month","rain")
-  tmp = sapply(requiredcols, match, colnames(clim), nomatch=0)
-  if (min(tmp)==0) {
-      return("Error:Invalid Climate Input") }
-  if (min(clim$rain < 0)) {
-    return("Error:Invalid Climate Input") }
-
   #extract spring data and add a column on the table which is average daily temperature
   clim$tavg = (clim$tmin + clim$tmax)/2.0
   spring = subset(clim, clim$month %in% spring.months)
 
-  #compute values
+  #compute values, aggregate data so as to display the wettest year (most precip) and the coldest spring (lowest tavg)
   AgTemp=aggregate(spring$tavg, by=list(spring$year), mean) #aggregates the avg. temp data into average by year
   lowyear = AgTemp$Group.1[which.min(AgTemp$x)] #identifies the year with the coldest spring (average daily temp across 3 months)
-  
   spring.precip = aggregate(spring$rain, by=list(spring$year), sum)
-  WettestSpring = spring.precip$Year[which.max(spring.precip$Rain)]
+  WettestSpring = spring.precip$Group.1[which.max(spring.precip$x)]
   
-  return(list("Mean Spring Temp" = mean.springT, "Coldest Spring"=lowyear, 
-              "Mean Spring Rain"=MeanSpringRain, "Wettest Spring"=WettestSpring ))
+  if(Interval=="y") {
+    ColdYear=subset(spring,spring$year==lowyear, select=c(year,tavg))#subset "spring" so that the "year" is equal to the year with lowest spring temp
+    Coldyear=aggregate(ColdYear$tavg,by=list(ColdYear$year),mean)
+    RainYear=subset(spring,spring$year==WettestSpring, select=c(year,rain))
+    RainYear=aggregate(RainYear$rain,by=list(RainYear$year),sum)
+  }
+  
+  if(Interval=="m"){
+    ColdYear=subset(spring,spring$year==lowyear, select=c(year,month,tavg))#subset "spring" so that the "year" is equal to the year with lowest spring temp
+    ColdYear=aggregate(ColdYear$tavg,by=list(ColdYear$month, ColdYear$year),mean)
+    RainYear=subset(spring,spring$year==WettestSpring, select=c(year,month,rain))
+    RainYear=aggregate(RainYear$rain,by=list(RainYear$month,RainYear$year),sum)
+}
+
+if(Interval=="d"){
+  ColdYear=subset(spring,spring$year==lowyear, select=c(year,month,day,tavg))#subset "spring" so that the "year" is equal to the year with lowest spring temp
+  RainYear=subset(spring,spring$year==WettestSpring, select=c(year,month,day,rain))
+}
+return(list(ColdYear,RainYear))
 }
